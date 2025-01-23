4e19e2d804e515f6b50f17101ab219761960e3fc
Anthony Tropeano <anthonytropeano@me.com>
2025-01-23 12:58:16 -0500

    Implementing a doubly linked, generic list in C.
    This data structure will be critical for use in
    the THREADS-Scheduler project.
    
    Created a Utils folder for generic, reusable code.
    This sits outside the THREADS-Scheduler project,
    but will be useful for upcoming projects 2-4.
    
    Just the prototypes for now. Will implement the
    functions in the next commit.

---
+++ Utils/LinkedList/LinkedList.c
@@ -0,0 +1,12 @@
+/**
+ * @file LinkedList.c
+ * @see LinkedList.h
+ * @author Anthony Tropeano
+ * @date  1/23/2025
+ *
+ * @brief This file contains the implementation for a generic doubly linked list.
+ */
+
+#ifndef LINKEDLIST_H
+#include "LinkedList.h"
+#endif
