748d64ea136aeeae56608c718ed5311782ab6a66
Anthony Tropeano <anthonytropeano@me.com>
2025-01-31 15:44:40 -0500

    Refactored previous solution for the 00 milestone.

--- THREADS-Scheduler/Utils/Strings/make.ps1
+++
@@ -1,9 +0,0 @@
-# Remove the existing /out directory if it exists
-Remove-Item -Recurse -Force .\out
-
-# Create a new /out directory
-New-Item -ItemType Directory -Path .\out
-
-# Compile LinkedList.c into an object file with warnings enabled, adding the Include directory
-gcc -Wall -c .\StringUtils.c -I ../../Include -o .\out\StringUtils.o
-
